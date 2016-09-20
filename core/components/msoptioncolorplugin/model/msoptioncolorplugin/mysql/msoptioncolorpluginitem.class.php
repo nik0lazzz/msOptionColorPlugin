279a1bd065b8ee58ea1dae4d5530724c2cbb0bd1
nik0lazzz <nik0lazzz@mail.ru>
2016-09-20 09:32:59 +0300

    Two commit

--- core/components/msoptioncolorplugin/model/msoptioncolorplugin/mysql/msoptioncolorpluginitem.class.php
+++ core/components/msoptioncolorplugin/model/msoptioncolorplugin/mysql/msoptioncolorpluginitem.class.php
@@ -1,6 +1,3 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . '/msoptioncolorpluginitem.class.php');
-
-class msOptionColorPluginItem_mysql extends msOptionColorPluginItem
-{
-}
+require_once (dirname(dirname(__FILE__)) . '/msoptioncolorpluginitem.class.php');
+class msOptionColorPluginItem_mysql extends msOptionColorPluginItem {}
