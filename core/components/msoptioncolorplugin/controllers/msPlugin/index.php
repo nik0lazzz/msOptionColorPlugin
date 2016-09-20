a539551f10a1e613df79d5acb0dd1033c6ba81c0
nik0lazzz <nik0lazzz@mail.ru>
2016-09-20 17:07:37 +0300

    Manager completed

--- core/components/msoptioncolorplugin/controllers/msPlugin/index.php
+++ core/components/msoptioncolorplugin/controllers/msPlugin/index.php
@@ -2,7 +2,7 @@
 
 return array(
     'map' => array(
-        'msProductData' => 'msOptionColorPlugin/core/components/msoptioncolorplugin/model/msoptioncolorplugin/msproductdata.inc.php',
+        'msProductData' => '/msOptionColorPlugin/core/components/msoptioncolorplugin/model/msoptioncolorplugin/msproductdata.inc.php',
     ),
     'manager' => array(
         'msProductData' => '/msOptionColorPlugin/assets/components/msoptioncolorplugin/js/mgr/ms_plugin/msproductdata.js',
